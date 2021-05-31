f88316748d1870ccd87caa33186250feb8a8b786
Capexoto <gustavo3x2@gmail.com>
2021-05-31 13:28:57 -0300

    teste de tempo corregido

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ function Home() {
             <Link href='/sobre'>
                 <a>Acessar página sobre</a>
             </Link>
+            <br/>
             <Link href='/tempo'>
                 <a>Acessar página tempo</a>
             </Link>
--- pages/tempo.js
+++ pages/tempo.js
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
 function Tempo () {
     const dynamicDate = new Date();
-    const dynamicDateString = dynamicDate.toGMTSring();
+    const dynamicDateString = dynamicDate.toGMTString();
 
     return(
         <div>
             {dynamicDateString} (dinâmico)
+            <br />
+            <Link href='/'>
+                <a>Acessar página Home</a>
+            </Link>
         </div>
     )
 }
