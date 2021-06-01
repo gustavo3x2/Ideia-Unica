b01e8ab9cbadada7acb340f7fe00c4cfa1eea4ec
Capexoto <gustavo3x2@gmail.com>
2021-06-01 16:18:17 -0300

    implementando .env

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function Tempo (request, response) {
+async function Tempo (request, response) {
+    const apiSecret = process.env.COVERTKIT_API_SECRET
     const dynamicDate = new Date();
 
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}&from=2016-02-01&to=2015-02-28`)
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     })
 }
 
